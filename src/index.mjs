779d0f07a6f4d64339b8ff398f2f76ca6d474249
Propromp <p@propromp.net>
2024-12-29 19:11:03 +0900

    fix: added association between models

--- src/index.mjs
+++ src/index.mjs
@@ -11,38 +11,24 @@ const original = {}
 const target = {}
 original.sequelize = new Sequelize(process.env.CMD_CONVERT_ORIGINAL_DB_URL);
 target.sequelize = new Sequelize(process.env.CMD_CONVERT_TARGET_DB_URL);
-original.user = user(original.sequelize, Sequelize);
-target.user = user(target.sequelize, Sequelize);
-original.note = note(original.sequelize, Sequelize);
-target.note = note(target.sequelize, Sequelize);
-original.revision = revision(original.sequelize, Sequelize);
-target.revision = revision(target.sequelize, Sequelize);
-original.author = author(original.sequelize, Sequelize);
-target.author = author(target.sequelize, Sequelize);
-
+const modelNames = ['User', 'Note', 'Revision', 'Author'];
+modelNames.forEach((modelName) => {
+    original[modelName] = require(`codimd/lib/models/${modelName}.js`)(original.sequelize, Sequelize)
+    target[modelName] = require(`codimd/lib/models/${modelName}.js`)(target.sequelize, Sequelize)
+})
+modelNames.forEach((modelName) => {
+    original[modelName].associate(original)
+    target[modelName].associate(target)
+})
 await target.sequelize.sync()
 console.log("Initialized.")
 
 // Conversion
-console.log("converting users.")
-const users = await original.user.findAll()
-await target.user.bulkCreate(users)
-console.log("converted users.")
-
-console.log("converting notes.")
-const notes = await original.note.findAll()
-await target.note.bulkCreate(notes)
-console.log("converted notes.")
-
-console.log("converting revisions.")
-
-const revisions = await original.revision.findAll()
-await target.revision.bulkCreate(revisions)
-console.log("converted revisions.")
-
-console.log("converting authors.")
-const authors = await original.author.findAll()
-await target.author.bulkCreate(authors)
-console.log("converted authors.")
+await modelNames.forEach(async (modelName) => {
+    console.log(`converting ${modelName}.`)
+    const data = await original[modelName].findAll()
+    await target[modelName].bulkCreate(data)
+    console.log(`converted ${modelName}.`)
+})
 
 console.log("Conversion complete.")
